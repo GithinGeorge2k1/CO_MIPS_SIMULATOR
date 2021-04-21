57522650447111172d69794c4cf970055eea0c07
GithinGeorge2k1 <cs19b046@iittp.ac.in>
2021-04-21 12:26:23 +0530

    figured a way for helpBox

--- about_help.cpp
+++ about_help.cpp
@@ -1,11 +1,26 @@
 #include "about_help.h"
 #include "ui_about_help.h"
+#include <QFile>
 
 About_Help::About_Help(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::About_Help)
 {
+
     ui->setupUi(this);
+
+    QFile html(":/Html/Html/index.html");
+    html.open(QFile::ReadOnly);
+    QString text=html.readAll();
+    ui->textEdit->setHtml(text);
+
+
+    QFile styleSheet(":/stylesheet/StyleSheet/new2.qss");
+    styleSheet.open(QFile::ReadOnly);
+    QString style=styleSheet.readAll();
+    ui->textEdit->setStyleSheet(style);
+
+    ui->textEdit->setReadOnly(true);
 }
 
 About_Help::~About_Help()
